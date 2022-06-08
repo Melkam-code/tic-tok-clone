af9b94801bbcecc0f2b59044aeb186c047802102
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2022-06-08 17:05:49 +0300

    Created and styled main footer components

--- tic-tok-clone/src/Video.js
+++ tic-tok-clone/src/Video.js
@@ -25,7 +25,7 @@ function Video() {
         onClick={handleVideoPress}
         ref={videoRef}
         className='video_player'>
-        <source src="https://v16-webapp.tiktok.com/64427e93d5f56fce5a7d87943c5a9b9c/62a0f273/video/tos/useast2a/tos-useast2a-pve-0068/16369cb46ed84b0382a349a7b2f1ed2d/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1004&bt=502&btag=80000&cs=0&ds=3&ft=eXd.6HQSMyq8ZS3sawe2NwDoyl7Gb&mime_type=video_mp4&qs=0&rc=NDo2OGY0OmRoZWc2ZmU2M0BpamhrNWU6ZnFmZDMzNzczM0BfMGJiNWEtNTExLWBhMjM1YSMuNXExcjQwZmNgLS1kMTZzcw%3D%3D&l=2022060813003901021713420608294037" type="video/mp4"/>
+        <source src="https://v16-webapp.tiktok.com/c6cd9f606f4de2d8d8a63051bde05b7e/62a0fe1b/video/tos/useast2a/tos-useast2a-pve-0068/d303534524e141deb8d9a3e2fc584f9a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2634&bt=1317&btag=80000&cs=0&ds=3&ft=eXd.6HQSMyq8ZK_uawe2NTPjyl7Gb&mime_type=video_mp4&qs=0&rc=ZTM7aTc1NzxlZ2ZnODo8aUBpM3ZtajY6ZnY3ZDMzNzczM0AvLjBeMTIxXjQxMjJhY2BeYSNqLWxzcjQwYGNgLS1kMTZzcw%3D%3D&l=202206081352500101890900661F2FA3C4" type="video/mp4"/>
         </video>
 
       {/* video footer */}
