c0bdf84588ab980b8804fff71b57276492f568af
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2022-06-08 16:26:35 +0300

    Preparing footer and sidebar for videos

---
+++ tic-tok-clone/src/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './VideoFooter.css'
+
+function VideoFooter() {
+  return (
+    <div className='video_footer'>
+     <div className="video_footer_text">
+         <h3>@melkam</h3>
+         <p>its me</p>
+     </div>
+    </div>
+  )
+}
+
+export default VideoFooter
