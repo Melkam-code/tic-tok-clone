5793dd40e43c551ec9966e6ea40ebf454e5d84a0
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2022-06-09 11:59:39 +0300

    Connecting to db with mongoose and testing api with local data

---
+++ tic-tok-backend/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        url: "https://v16-webapp.tiktok.com/14c02f65ecb24c42983798d16bfd8aae/62a1eba6/video/tos/useast2a/tos-useast2a-ve-0068c002/9d912473b22047c88ab296e55ca5d34a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=924&bt=462&btag=80000&cs=0&ds=3&ft=eXd.6HQSMyq8ZfN_awe2NiS0yl7Gb&mime_type=video_mp4&qs=0&rc=Zjc0aGhmNjc8OWRoNjs0NEBpamQ3bmc6Zmd0PDMzNzczM0BfX2E1NGNfNmAxXmNfLjBeYSNsXzQvcjQwLTVgLS1kMTZzcw%3D%3D&l=202206090645280101901860291B099013",
+        channel: "rafelredma",
+        song: "Waste my time on you",
+        likes: 3521,
+        messages: 899,
+        description: "Epic experience",
+        shares: 320
+    },
+    {
+        url: "https://v16-webapp.tiktok.com/14c02f65ecb24c42983798d16bfd8aae/62a1eba6/video/tos/useast2a/tos-useast2a-ve-0068c002/9d912473b22047c88ab296e55ca5d34a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=924&bt=462&btag=80000&cs=0&ds=3&ft=eXd.6HQSMyq8ZfN_awe2NiS0yl7Gb&mime_type=video_mp4&qs=0&rc=Zjc0aGhmNjc8OWRoNjs0NEBpamQ3bmc6Zmd0PDMzNzczM0BfX2E1NGNfNmAxXmNfLjBeYSNsXzQvcjQwLTVgLS1kMTZzcw%3D%3D&l=202206090645280101901860291B099013",
+        channel: "rafelredma",
+        song: "Waste my time on you",
+        likes: 3521,
+        messages: 899,
+        description: "Epic experience",
+        shares: 320
+    },
+    {
+        url: "https://v16-webapp.tiktok.com/14c02f65ecb24c42983798d16bfd8aae/62a1eba6/video/tos/useast2a/tos-useast2a-ve-0068c002/9d912473b22047c88ab296e55ca5d34a/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=924&bt=462&btag=80000&cs=0&ds=3&ft=eXd.6HQSMyq8ZfN_awe2NiS0yl7Gb&mime_type=video_mp4&qs=0&rc=Zjc0aGhmNjc8OWRoNjs0NEBpamQ3bmc6Zmd0PDMzNzczM0BfX2E1NGNfNmAxXmNfLjBeYSNsXzQvcjQwLTVgLS1kMTZzcw%3D%3D&l=202206090645280101901860291B099013",
+        channel: "rafelredma",
+        song: "Waste my time on you",
+        likes: 3521,
+        messages: 899,
+        description: "Epic experience",
+        shares: 320
+    },
+]
+
+export default data
--- tic-tok-backend/server.js
+++ tic-tok-backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import data from './data.js'
 
 //configure app
 const app = express();
@@ -7,8 +8,22 @@ const app = express();
 //define port for app to run
 const port = 9000;
 
+//middleware
+
+//db config
+const connection_url = "mongodb+srv://melkam:melkam@cluster1.b8cq6c6.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+//api endpoints
 //health check api
 app.get('/', (req, res) => res.status(200).send('Hello world!'));
+
+//test api to see local data showup on endpoint
+app.get('/v1/posts', (req, res) => res.status(200).send(data));
 
 //make app listen to port
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
